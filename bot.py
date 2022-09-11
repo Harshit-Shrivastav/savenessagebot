8d6de821f8108bb4a4fe454c23f28a1417deb3a9
Harshit Shrivastav <87630299+Harshit-Shrivastav@users.noreply.github.com>
2022-09-11 22:19:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,9 +15,7 @@ logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
 )
 
-bot = TelegramClient(None, api_id=6, api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e").start(
-    bot_token=config("BOT_TOKEN")
-)
+bot = TelegramClient(None, api_id=6, api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e").start(phone)
 
 logging.info("Starting bot...")
 
